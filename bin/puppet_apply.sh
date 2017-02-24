2d802dcf433ec02aac320b50d8fb66c43857a319
Ben Dalling <b.dalling@locp.co.uk>
2017-02-24 22:03:21 +0000

    WIP

--- bin/puppet_apply.sh
+++ bin/puppet_apply.sh
@@ -3,7 +3,7 @@
 # A small script to run puppet with the detailed exit codes attribute.  In
 # this case an exit code of 2 is a successful change.
 #############################################################################
-FACTER_REGION=$AWS_REGION puppet apply environment/apply.pp --test
+FACTER_REGION=$AWS_REGION puppet apply $1 --test
 status=$?
 
 case $status in
