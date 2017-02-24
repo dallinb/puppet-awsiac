033988bd5a8a99e5b18644e103ea2e90d06e1c40
Ben Dalling <b.dalling@locp.co.uk>
2017-02-24 22:01:33 +0000

    Add destroy script.

---
+++ environment/destroy.pp
@@ -0,0 +1,22 @@
+include stdlib
+
+if $::prefix == undef {
+  fail('You must specify a prefix.')
+}
+
+if $::region == undef {
+  fail('You must specify a region.')
+}
+
+$vpc = regsubst(upcase("${prefix}${region}"), '-([A-Z]).*(\d+)$', '\1\2')
+
+$tags = {
+  environment => $vpc,
+}
+
+ec2_vpc { $vpc:
+  ensure     => absent,
+  region     => $::region,
+  cidr_block => '10.0.0.0/16',
+  tags       => $tags,
+}
