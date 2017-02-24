2ffe8c1730928a7e4d0187b294f0f533f6246b32
Ben Dalling <b.dalling@locp.co.uk>
2017-02-24 21:21:12 +0000

    WIP

--- bin/puppet_apply.sh
+++ bin/puppet_apply.sh
@@ -4,7 +4,7 @@
 # this case an exit code of 2 is a successful change.
 #############################################################################
 FACTER_REGION=$AWS_REGION puppet apply environment/apply.pp --test \
-  --detailed-exitcodes
+  --logdest=${CIRCLE_ARTIFACTS}/puppet.json
 status=$?
 
 if $status == 2; then
