fa69fa7477b2a3717422126b28bef82bb3f5ecf7
Ben Dalling <b.dalling@locp.co.uk>
2017-02-25 00:28:40 +0000

    WIP

--- environment/destroy.pp
+++ environment/destroy.pp
@@ -10,24 +10,6 @@ if $::region == undef {
 
 $vpc = regsubst(upcase("${prefix}${region}"), '-([A-Z]).*(\d+)$', '\1\2')
 
-Ec2_instance <| |> -> Ec2_securitygroup <| |>
-Ec2_instance <| |> -> Ec2_vpc_subnet <| |>
-Ec2_securitygroup <| |> -> Ec2_vpc <| |>
-Ec2_vpc_subnet <| |> -> Ec2_vpc <| |>
-Ec2_vpc_subnet <| |> -> Ec2_vpc_internet_gateway <| |>
-Ec2_vpc_subnet <| |> -> Ec2_vpc_routetable <| |>
-Ec2_vpc_internet_gateway <| |> -> Ec2_vpc <| |>
-Ec2_vpc_routetable <| |> -> Ec2_vpc <| |>
-Ec2_vpc <| |> -> Ec2_vpc_dhcp_options <| |>
-
 ec2_vpc { $vpc:
   ensure     => absent,
-  region     => $::region,
-  cidr_block => '10.0.0.0/16',
-}
-
-ec2_vpc_internet_gateway { $vpc:
-  ensure => absent,
-  region => $::region,
-  vpc    => $vpc,
 }
