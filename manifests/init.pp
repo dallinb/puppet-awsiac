044b86bb503d988dbb9f015f96f75a9bb0dc95a2
Ben Dalling <b.dalling@locp.co.uk>
2017-05-17 18:19:03 +0100

    WIP

---
+++ manifests/init.pp
@@ -0,0 +1,65 @@
+# Class: awsiac
+# ===========================
+#
+# Full description of class awsiac here.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'awsiac':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2017 Your name here, unless otherwise noted.
+#
+class awsiac (
+  $region,
+  $vpc_prefix,
+  $ensure     = present,
+  $cidr_block = '172.16.0.0/12',
+  ){
+  include aws
+
+  $vpc = regsubst(upcase("${vpc_prefix}${region}"), '-([A-Z]).*(\d+)$', '\1\2')
+
+  $tags = {
+    environment => downcase($vpc),
+  }
+
+  ec2_vpc { $vpc:
+    ensure     => $ensure,
+    region     => $region,
+    cidr_block => '10.0.0.0/16',
+    tags       => $tags,
+  }
+}
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+describe 'awsiac' do
+  let(:pre_condition) do
+    [
+      'class aws () {}',
+      'define ec2_vpc (
+        $ensure,
+        $region,
+        $cidr_block,
+        $tags) {}'
+    ]
+  end
+
+  context 'with default values for all parameters' do
+    it { should raise_error(Puppet::Error) }
+  end
+
+  context 'with the region set' do
+    let :params do
+      {
+        region: 'eu-west-2',
+        vpc_prefix: 'test'
+      }
+    end
+
+    it { should contain_class('awsiac') }
+    it { should contain_class('aws') }
+    it { should contain_ec2_vpc('TESTEUW2') }
+  end
+end
