c92b3a6b8a999c46352126334f1de40339950d7b
Ben Dalling <b.dalling@locp.co.uk>
2017-06-07 19:10:21 +0100

    Specify the module path.

--- puppet_wrapper.sh
+++ puppet_wrapper.sh
@@ -11,7 +11,9 @@ echo "Puppet Environment: $PUPPET_ENVIRONMENT"
 echo "VPC Prefix        : $FACTER_vpc_prefix"
 echo "VPC Postfix       : $FACTER_vpc_postfix"
 
-FACTER_region=$AWS_REGION puppet apply examples/init.pp --test --environment $PUPPET_ENVIRONMENT --debug
+FACTER_region=$AWS_REGION puppet apply examples/init.pp --test \
+  --environment $PUPPET_ENVIRONMENT \
+  --modulepath /home/ubuntu/.puppetlabs/etc/code/modules --debug
 status=$?
 [ $status -eq 0 -o $status -eq 2 ] && exit 0
 exit $status
